d0f22b4b3fd47c8186c427555c6a174257323596
Hal Blackburn <hwtb2@cam.ac.uk>
2019-04-02 17:55:08 +0100

    Refactor to support wide usage patterns
    As well as being a context manager, dynamic_entrypoint() can now be
    used as a function decorator, and via start()/stop() calls. The context
    manager is now re-entrant.

--- test_prybar.py
+++ test_prybar.py
@@ -5,7 +5,27 @@ import pkg_resources
 
 import pytest
 
+import prybar
 from prybar import dynamic_entrypoint
+
+
+@pytest.fixture(autouse=True)
+def validate_entrypoint_cleanup():
+    for key, dist in pkg_resources.working_set.by_key.items():
+        if dist.location == prybar.__file__:
+            pytest.fail(f'a dist created by prybar exists before running a '
+                        f'test: key: {key}, dist: {dist}')
+    if prybar.__file__ in pkg_resources.working_set.entries:
+        pytest.fail('prybar.py in working_set.entries before running a test')
+
+    yield
+
+    for key, dist in pkg_resources.working_set.by_key.items():
+        if dist.location == prybar.__file__:
+            pytest.fail(f'a dist created by prybar exists after running a '
+                        f'test: key: {key}, dist: {dist}')
+    if prybar.__file__ in pkg_resources.working_set.entries:
+        pytest.fail('prybar.py in working_set.entries after running a test')
 
 
 nested_ep = None
@@ -29,20 +49,41 @@ def create_nested_ep():
         yield ep
 
 
-def test_dynamic_entrypoint_registers_entrypoint():
+def test_dynamic_entrypoint_registers_entrypoint_via_with():
     assert list(pkg_resources.iter_entry_points('test-group')) == []
 
     with dynamic_entrypoint('test-group', ep_1):
         eps = list(pkg_resources.iter_entry_points('test-group'))
         assert len(eps) == 1
         assert eps[0].name == 'ep_1'
+        assert eps[0].load() is ep_1
+
+    assert list(pkg_resources.iter_entry_points('test-group')) == []
 
 
-def test_dynamic_entrypoint_unregisters_entrypoint():
-    with dynamic_entrypoint('test-group', ep_1):
+def test_dynamic_entrypoint_registers_entrypoint_via_decorator():
+    assert list(pkg_resources.iter_entry_points('test-group')) == []
+
+    @dynamic_entrypoint('test-group', ep_1)
+    def func():
         eps = list(pkg_resources.iter_entry_points('test-group'))
         assert len(eps) == 1
         assert eps[0].name == 'ep_1'
+        assert eps[0].load() is ep_1
+
+    assert list(pkg_resources.iter_entry_points('test-group')) == []
+
+
+def test_dynamic_entrypoint_registers_entrypoint_via_start():
+    assert list(pkg_resources.iter_entry_points('test-group')) == []
+
+    dep = dynamic_entrypoint('test-group', ep_1)
+    dep.start()
+    eps = list(pkg_resources.iter_entry_points('test-group'))
+    assert len(eps) == 1
+    assert eps[0].name == 'ep_1'
+    assert eps[0].load() is ep_1
+    dep.stop()
 
     assert list(pkg_resources.iter_entry_points('test-group')) == []
 
@@ -67,10 +108,9 @@ def test_names_must_be_unique_per_scope():
 
     assert (str(excinfo.value) ==
             "'foo' is already registered under 'test-group' in scope "
-            "'prybar.dynamic'")
+            "'prybar.scope.default'")
 
 
-# TODO: test shadowing existing scope
 def test_scope_cant_shadow_existing_distribution():
     with pytest.raises(ValueError) as excinfo:
         # Try to shadow pytest
@@ -144,21 +184,137 @@ def test_valid_arguments(args, kwargs):
                             pkg_resources.iter_entry_points('test-group')]
 
 
-@pytest.mark.parametrize('args, kwargs, exc', [
-    (['test-group'], {}, ValueError),
-    (['test-group', None], {}, ValueError),
+@pytest.mark.parametrize('args, kwargs, exc, msg', [
+    ([42], {}, TypeError,
+     'group must be a string, got: 42'),
+
+    (['test-group'], {}, TypeError,
+     'name and module_name must be specified when entrypoint is not '
+     'specified'),
+
+    (['test-group', None], {}, TypeError,
+     'name and module_name must be specified when entrypoint is not '
+     'specified'),
+
     # EntryPoint instance with existing dist
     (['test-group', pkg_resources.EntryPoint('ep_1', __name__,
                                              attrs=('ep_1',), dist=object())],
-     {}, ValueError),
+     {}, ValueError, 'can\'t specify a pkg_resources.Entrypoint instance with '
+                     'a dist already attached'),
     # Specifying things twice
-    (['test-group', f'ep_1 = {__name__}:ep_1'], dict(name='abc'), ValueError),
-    (['test-group', f'ep_1 = {__name__}:ep_1'], dict(module='abc'),
-     ValueError),
+    (['test-group', f'ep_1 = {__name__}:ep_1'], dict(name='abc'), TypeError,
+     'can\'t specify name, module_name or attribute when entrypoint is a '
+     'pkg_resources.Entrypoint'),
+    (['test-group', f'ep_1 = {__name__}:ep_1'], dict(module='abc'), TypeError,
+     'can\'t specify name, module_name or attribute when entrypoint is a '
+     'pkg_resources.Entrypoint'),
     (['test-group', f'ep_1 = {__name__}:ep_1'], dict(attribute='abc'),
-     ValueError),
+     TypeError, 'can\'t specify name, module_name or attribute when '
+                'entrypoint is a pkg_resources.Entrypoint'),
 ])
-def test_invalid_arguments(args, kwargs, exc):
-    with pytest.raises(exc):
+def test_invalid_arguments(args, kwargs, exc, msg):
+    with pytest.raises(exc) as excinfo:
         with dynamic_entrypoint(*args, **kwargs):
             pass
+
+    assert msg in str(excinfo.value)
+
+
+def test_cant_exit_without_enter():
+    with pytest.raises(RuntimeError) as excinfo:
+        dynamic_entrypoint('test-group', ep_1).__exit__(None, None, None)
+    assert str(excinfo.value) == '__exit__() called more than __enter__()'
+
+
+def test_cant_use_stop_with_context_manager():
+    dep = dynamic_entrypoint('test-group', ep_1)
+    with pytest.raises(RuntimeError) as excinfo:
+        with dep:
+            dep.stop()
+    assert (str(excinfo.value) ==
+            'can\'t stop() while active via __enter__()')
+
+
+def test_cant_use_start_with_context_manager():
+    dep = dynamic_entrypoint('test-group', ep_1)
+    with pytest.raises(RuntimeError) as excinfo:
+        with dep:
+            dep.start()
+    assert (str(excinfo.value) ==
+            'can\'t start() while active via __enter__()')
+
+
+def test_cant_use_stop_with_decorator():
+    dep = dynamic_entrypoint('test-group', ep_1)
+    @dep
+    def block():
+        with pytest.raises(RuntimeError) as excinfo:
+            dep.stop()
+        assert (str(excinfo.value) ==
+                'can\'t stop() while active via __enter__()')
+
+
+def test_cant_use_start_with_decorator():
+    dep = dynamic_entrypoint('test-group', ep_1)
+    @dep
+    def block():
+        with pytest.raises(RuntimeError) as excinfo:
+            dep.start()
+        assert (str(excinfo.value) ==
+                'can\'t start() while active via __enter__()')
+
+
+def test_cant_use_context_manager_with_start():
+    dep = dynamic_entrypoint('test-group', ep_1)
+    dep.start()
+
+    with pytest.raises(RuntimeError) as excinfo:
+        with dep:
+            pytest.fail('should not enter block')
+    dep.stop()
+
+    assert str(excinfo.value) == 'can\'t __enter__() while active via start()'
+
+
+def test_cant_use_context_manager_with_decorator():
+    dep = dynamic_entrypoint('test-group', ep_1)
+    dep.start()
+
+    @dep
+    def foo():
+        pytest.fail('should not enter function')
+
+    with pytest.raises(RuntimeError) as excinfo:
+        foo()
+    dep.stop()
+
+    assert str(excinfo.value) == 'can\'t __enter__() while active via start()'
+
+
+def test_start_stop_can_be_called_when_already_started_or_stopped():
+    dep = dynamic_entrypoint('test-group', ep_1)
+    assert list(pkg_resources.iter_entry_points('test-group')) == []
+    dep.stop()
+    dep.stop()
+    dep.start()
+    assert len(list(pkg_resources.iter_entry_points('test-group'))) == 1
+    dep.start()
+    dep.start()
+    assert len(list(pkg_resources.iter_entry_points('test-group'))) == 1
+    dep.stop()
+    dep.stop()
+    assert list(pkg_resources.iter_entry_points('test-group')) == []
+
+
+def test_scopes_are_normalised_by_pkg_resources():
+    # scopes are pkg_resources distributions, and underscores are normalised
+    # to hyphens in distribution names.
+    with dynamic_entrypoint('test-group', entrypoint=ep_1, scope='foo-bar'):
+        with pytest.raises(ValueError) as excinfo:
+            with dynamic_entrypoint('test-group', entrypoint=ep_1,
+                                    scope='foo_bar'):
+                pass
+
+    assert str(excinfo.value) == (
+        "'ep_1' is already registered under 'test-group' in scope "
+        "'foo_bar' ('foo-bar')")
